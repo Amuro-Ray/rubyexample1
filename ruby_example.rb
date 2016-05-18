f71d438b74ab213e8f0a386660c09d6568657b29
Jason Carty <Jcarty>
2016-05-18 07:57:05 +0100

    Example
    example

---
+++ ruby_example.rb
@@ -0,0 +1,16 @@
+class Pieshop
+    def initialize()
+        unless (defined? @instance) then
+            
+            @instance = "new_address"
+        end
+    end
+    otherclass_method
+
+    second_class_method
+
+
+    def somemethod
+        
+    end
+end
